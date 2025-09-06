7caedd388463e6765408d2ac330390edeb7d500d
brandonvasm <brandonmonzon2005@gmail.com>
2025-09-06 15:19:56 -0600

    feat: add Order model with FK to Customer

--- hw05/core/models.py
+++ hw05/core/models.py
@@ -6,3 +6,11 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    order_date = models.DateField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="orders")
+
+    def __str__(self):
+        return f"Order {self.id} - {self.customer.name}"
