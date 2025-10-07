64dc2d39122aa0c53cabd47ba5ed1c0b938c9d5f
brandonvasm <brandonmonzon2005@gmail.com>
2025-10-06 22:18:46 -0600

    chore: stop tracking hw05/hw05 pycache files

--- hw05/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Order, Product, OrderDetail
-
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Product)
-admin.site.register(OrderDetail)
